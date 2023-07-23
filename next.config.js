1a2631b7c9f18c6a7deda64111042a9662a0c6a7
hyunwoomemo <archihw94@gmail.com>
2023-07-23 22:53:36 +0900

    etc: 초기 셋팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  output: 'export',
+  images: { unoptimized: true } ,
+}
 
-module.exports = nextConfig
+const rehypePrettyCode = require('rehype-pretty-code');
+const fs = require('fs');
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  highlight: true,          // Enable word highlighting
+  lineNumber: true,         // Show line numbers
+  highlightLines: [2, 4],   // Highlight specific lines (optional)
+  highlightLanguage: 'jsx', // Specify the language for highlighting (optional)
+};
+ 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [[rehypePrettyCode, options]],
+  },
+});
+
+module.exports = {
+  ...nextConfig,
+  ...withMDX
+}
