68a95e7abe7ee8065219c71ff9891f37c524306b
hyunwoomemo <archihw94@gmail.com>
2023-07-28 08:53:08 +0900

    fix: 빌드 에러, react-router-dom 가져오는 코드 삭제

--- app/components/TIL/TILHeader.tsx
+++ app/components/TIL/TILHeader.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Link from 'next/link'
 import React, { ChangeEvent, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
 import { styled } from 'styled-components'
 
 interface Props {
