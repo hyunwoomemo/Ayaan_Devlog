9fac92b9f9333c5764e1e888cd75cc95e9dbb756
hyunwoomemo <archihw94@gmail.com>
2023-07-30 02:24:45 +0900

    refactor: 메인페이지에 blog 포스트 보여주기

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,44 @@
 // app/page.tsx
+import { getAllPosts, getCategories } from '@/lib/api';
+import Link from 'next/link';
+import PostPage from './components/PostPage';
+import Image from 'next/image';
+import PostItem from './components/PostItem';
+import Category from './components/Category';
 
-import Home from './components/Home';
+interface Post { 
+  id: string;
+  date: string;
+  title: string;
+  html: string;
+  category: string;
+}
 
 
 export default async function Page() {
+  const posts = await getAllPosts();
+  const categories = await getCategories();
   return (
-    <Home>
-      Ayaan 블로그 준비 중
-    </Home>
+    <PostPage>
+      {/* <Category categories={categories} /> */}
+      <ul>
+      {posts?.map((post) => {
+          const { id, date, title, category, summary } = post;
+        return (
+            <PostItem key={id} hide={false}>
+              <Link href={`/blog/${category}/${id}`} key={id}>
+              <li>{category}</li>
+              <li>{title}</li>
+              <li>{summary}</li>
+                <div>
+                  <li>{date}</li>
+                </div>
+              </Link>
+            </PostItem>
+          );
+        })}
+      </ul>
+      </PostPage>
   );
 }
 
